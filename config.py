ce1e42dae6c68c8e8c4badb9e9678ed8a03972e2
Zeinn0 <127055306+Zeinn0@users.noreply.github.com>
2023-04-17 13:47:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = environ.get("API_HASH","038b0b405782d91cbb32f60e917c1c6e")
 API_ID1 = int(environ.get("API_ID1","10729094"))
 API_HASH1 = environ.get("API_HASH1","038b0b405782d91cbb32f60e917c1c6e")
 sudoers = [int(x) for x in environ.get("sudoers", "").split()]
-LOG_GROUP_ID = environ.get("LOG_GROUP_ID","-1001615880226")
+LOG_GROUP_ID = environ.get("LOG_GROUP_ID","-1001889013916")
 BASE_DB = environ.get("BASE_DB","mongodb+srv://vampir:Al2552001@cluster0.yeaoc.mongodb.net/vampir?retryWrites=true&w=majority")
 MONGO_URL = environ.get("MONGO_URL","mongodb+srv://vampir:Al2552001@cluster0.yeaoc.mongodb.net/vampir?retryWrites=true&w=majority")
 ARQ_API_URL = environ.get("ARQ_API_URL","https://arq.hamker.in")
